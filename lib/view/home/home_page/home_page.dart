38b466c327b28370e6de6b12e54a500cc17b90b5
omer <46028769+sariomer96@users.noreply.github.com>
2025-08-27 11:59:15 +0300

    home page added

---
+++ lib/view/home/home_page/home_page.dart
@@ -0,0 +1,99 @@
+import 'package:flutter/material.dart';
+
+class HomePage extends StatefulWidget {
+  const HomePage({super.key});
+
+  @override
+  State<HomePage> createState() => _HomePageState();
+}
+
+class _HomePageState extends State<HomePage> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: const Text('Todo'),
+      ),
+      body: Column(
+        children: [_searchFilter(), const TaskListView()],
+      ),
+      floatingActionButton: FloatingActionButton(
+        onPressed: (){},
+        child: const Icon(Icons.add),
+        ),
+        
+    );
+  }
+
+  Row _searchFilter() {
+    return Row(
+      children: [
+        Expanded(
+          child: Padding(
+            padding: const EdgeInsets.all(8.0),
+            child: SearchAnchor(
+              builder: (BuildContext context, SearchController controller) {
+                return SearchBar(
+                  controller: controller,
+                  padding: const WidgetStatePropertyAll<EdgeInsets>(
+                    EdgeInsets.symmetric(horizontal: 16.0),
+                  ),
+                  leading: const Icon(Icons.search),
+                );
+              },
+              suggestionsBuilder:
+                  (BuildContext context, SearchController controller) {
+                return List<ListTile>.generate(5, (int index) {
+                  final String item = 'item $index';
+                  return ListTile(
+                    title: Text(item),
+                    onTap: () {
+                      setState(() {
+                        controller.closeView(item);
+                      });
+                    },
+                  );
+                });
+              },
+            ),
+          ),
+        ),
+        IconButton(
+          icon: const Icon(Icons.filter_list),
+          onPressed: () {},
+        ),
+      ],
+    );
+  }
+}
+
+class TaskListView extends StatefulWidget {
+  const TaskListView({super.key});
+
+  @override
+  State<TaskListView> createState() => _TaskListViewState();
+}
+
+class _TaskListViewState extends State<TaskListView> {
+  List<String> tasks = List.generate(5, (index) => 'Görev ${index + 1}');
+  @override
+  Widget build(BuildContext context) {
+    return Expanded(
+      child: Padding(
+        padding: const EdgeInsets.all(8.0),
+        child: ListView.builder(
+          itemCount: tasks.length,
+          itemBuilder: (context, index) {
+            return ListTile(
+              leading: CircleAvatar(
+                child: Text('${index + 1}'),
+              ),
+              title: Text(tasks[index]),
+              
+            );
+          },
+        ),
+      ),
+    );
+  }
+}
